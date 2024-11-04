65e04a2f33adf296583fae5a79234d47ebd303d7
Sebin Simon <sebin.sn@pitsolution.com>
2024-11-04 14:18:03 -0800

    Add scss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+// const path = require("path");
 
 const withNextIntl = createNextIntlPlugin();
+
+// Get directory name using ES Module approach
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@import "_partials/variables";`,
+  },
+};
 
 export default withNextIntl(nextConfig);
